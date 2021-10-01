00a3cd42ca00112c58ff951efac600699802d57f
sekhar76405 <ssjio5402@gmail.com>
2021-10-01 16:11:07 +0530

    commented

--- main/Transaction.cpp
+++ main/Transaction.cpp
@@ -27,6 +27,16 @@ long Ledger_size =0;
 long Mempool_size =0;
 long Transaction_Ids =0;
 
+//using a stack to store number of trans happend till now
+//currently just storing the trans as int type denoting no.of.trans happended till a point = size of stack;
+//currently this can also be substituted by a simple int varialble total_trans and we keep doing +1 every time new transaction happens.
+//stack<int> List_of_Transaction_ID;
+
+// Future goals:
+// -----Try to design this stack template of our own Transaction class,
+// -----to directly store the objects of transaction happened till time.
+// -----then to get the id of a transaction we would do --> List_of_Transactions.top().get_Transaction_ID();
+
 void Transaction::set_Transaction(string sender, string reciever, long amount, long Transaction_ID)
     {
         this->Transaction_ID = Transaction_ID;
@@ -56,3 +66,8 @@ void Transaction::set_Transaction(string sender, string reciever, long amount, l
     {
         return transaction_msg;
     }
+
+// having an array to save the trantransaction till time. (Max transactions hardcoded as N = 100 in the macro)
+// this array will be using 1 based indexing
+//Transaction Transaction_Array[N];
+
