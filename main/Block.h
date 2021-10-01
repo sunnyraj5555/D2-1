b667bf7eda94ce55989d8e6ee288cc8f8375aa5f
sekhar76405 <ssjio5402@gmail.com>
2021-10-01 16:19:48 +0530

    designed Block class

--- main/Block.h
+++ main/Block.h
@@ -15,5 +15,41 @@ using namespace std;
 #define BLOCK_H
 
 
+class Block
+{
+    private:
+
+    long BlockNumber;       //every block has a number
+    Transaction Transactions_in_the_Block[N];       // to store the list of transactions in array of transaction class objects for each transaction
+    int No_of_Transactions_in_Block;            // to store how many transactions are there inside a particular block
+    string Previous_Hash, Block_Hash;          // to store prev has and current block hash inside 
+
+    public:
+
+    Block();
+    
+    void set_Previous_Hash();           // to set previous hash 
+
+    void set_Block_Hash(string Block_Hash);       // will set the hash of current block
+
+    void set_BlockNumber();         //  will set the block number accordingly from the current blockchain
+
+    string get_Previous_Hash(); 
+
+    string get_Block_Hash();
+
+    long get_BlockNumber();
+
+    long get_No_of_Transactions_in_Block(); 
+
+    // flush all the transaction objs from Transaction_Array (exists in Transaction.h/.cpp) into Transactions_in_the_Block[] 
+    // also updating the value of No_of_Transactions_in_Block variable = Transaction_Array size
+    void fill_transactions_in_the_block();      
+
+    // 
+    string get_transaction_msg_from_block(int i);
+
+    long get_transaction_ID_from_block(int i);
+};
 
 #endif /* BLOCK_H */
