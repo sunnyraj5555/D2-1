fe85dc17d884de3a44d5f5aa0c9c2aacde2d59eb
sekhar76405 <ssjio5402@gmail.com>
2021-10-02 16:44:12 +0530

    defined Org class functions

--- main/Organization.cpp
+++ main/Organization.cpp
@@ -19,3 +19,39 @@
 
 using namespace std;
 
+
+void Organization::set_org_details(string org_name, string org_address, string org_phone_number ){
+        this->org_name = org_name;
+        this->org_address=org_address;
+        this->org_phone_number=org_phone_number; 
+    }
+    void Organization::get_org_details(){
+        cout<< "-----ORGANIZATION DETAILS------ "<<"\n";
+        cout<<"Name:"<< org_name <<endl;
+        cout<<"Address:"<<org_address <<endl;
+        cout<<"Phone Number:"<<org_phone_number <<endl;
+    }
+    
+    void Organization::set_Org_ID(){
+        this->Org_ID = OrgList_size + 1;
+    }
+    long Organization::get_Org_ID(){
+        return Org_ID;
+    }
+    
+    string Organization::get_org_name(){
+        return org_name;
+    }
+    
+    string Organization::get_org_address(){
+        return org_address;
+    }
+    string Organization::get_org_phone_number(){
+        return org_phone_number;
+    }
+    void Organization::set_registration_number(long registration_number){
+        this->registration_number=registration_number;
+    }
+    long Organization::get_registration_number(){
+        return registration_number;
+    }
