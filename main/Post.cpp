6ab6b4188bae65b73c6c21a7eb82e66091ddf124
sekhar76405 <ssjio5402@gmail.com>
2021-10-03 16:52:32 +0530

    defined  Post Class Functions

--- main/Post.cpp
+++ main/Post.cpp
@@ -19,53 +19,80 @@
 
 using namespace std;
 
+   void Post::set_Post_ID(){
+        Post_ID = PostList_size + 1;
+    }
+    
+    long Post::get_Post_ID(){
+        return Post_ID;
+    }
+    
+    void Post::set_post_details(string Post_Title, string Post_Description){
+        this->Post_Title = Post_Title;
+        this->Post_Description = Post_Description;
+    }
+    
+    void Post::set_org_details(long Org_ID, string org_name, string org_address, string org_phone_number){
+        this->Org_ID = Org_ID;
+        this->org_name = org_name;
+        this->org_address = org_address;
+        this->org_phone_number = org_phone_number;
+    }
+    
+    void Post::set_user_details(long User_ID, string user_name, string patient_name){
+        this->User_ID = User_ID;
+        this->user_name = user_name;
+        this->patient_name = patient_name;
+    }
+    
+    string Post::get_Post_Title(){
+        return Post_Title;
+    }
+    string Post::get_Post_Description(){
+        return Post_Description;
+    }
+    
+    
+    void Post::get_user_details(){
+        cout<<"\tPosted By: "<<user_name<<endl;
+        cout<<"\t      Uid: "<<User_ID<<endl;
+    }
+    
+    string Post::get_patient_name(){
+        return patient_name;
+    }
+    
+    void Post::get_org_details(){
+        
+        cout<<"\t  Org ID: "<< Org_ID <<endl;
+        cout<<"\tOrg Name: "<< org_name <<endl;
+        cout<<"\t Address: "<< org_address <<endl;
+        cout<<"\tHelpline: "<< org_phone_number <<endl;
+    }
 
-class Post {
+    void Post::set_amount(long amount_requested){
+        
+        this->amount_requested=amount_requested;
+        amount_collected = 0;
+        amount_left = amount_requested;
+    }
     
-    private:
+    void Post::deduct_money(long amount_received){
+        amount_collected += amount_received;
+        amount_left -= amount_received;
+    }
     
-    long Post_ID;
-    string Post_Title, Post_Description;
-    long amount_requested, amount_collected, amount_left;
+    long Post::get_amount_requested(){
+        return amount_requested;
+    }
     
-    long User_ID;
-    string user_name, patient_name;
+    long Post::get_amount_collected(){
+        return amount_collected;
+    }
     
-    long Org_ID;
-    string org_phone_number;
-    string org_name, org_address;
-    
-    public:
-    
-    void set_Post_ID();
-    
-    long get_Post_ID();
-    
-    void set_post_details(string Post_Title, string Post_Description);
-    
-    void set_org_details(long Org_ID, string org_name, string org_address, string org_phone_number);
-    
-    void set_user_details(long User_ID, string user_name, string patient_name);
-    
-    string get_Post_Title();
-    
-    string get_Post_Description();
-    
-    void get_user_details();
-    
-    string get_patient_name();
-    
-    void get_org_details();
-
-    void set_amount(long amount_requested);
-    
-    void deduct_money(long amount_received);
-    
-    long get_amount_requested();
-    
-    long get_amount_collected();
-    
-    long get_amount_left();
+    long Post::get_amount_left(){
+        return amount_left;
+    }
 
 
-};
+
