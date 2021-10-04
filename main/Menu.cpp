d825b98f22af6376e4bf8fe586334c469ecb3a55
sekhar76405 <ssjio5402@gmail.com>
2021-10-04 17:06:10 +0530

    admin_menu() defined

--- main/Menu.cpp
+++ main/Menu.cpp
@@ -48,3 +48,49 @@ void main_menu(){
     }
 
 }
+
+
+void admin_main_menu(){
+    while(1){
+        
+    cout<<endl;
+    cout<<"Welcome ADMIN.! Please select one of the following operations: "<<endl;
+    cout<<"1. Mine a Block "<<endl;
+    cout<<"2. Display BlockChain "<<endl;
+    cout<<"3. Display Ledger "<<endl;
+    cout<<"4. Display Mempool "<<endl;
+    cout<<endl;
+    cout<<"Enter 0 to return to main menu. "<<endl;
+    int choose;
+    cout<<"\nEnter your choice: ";
+    cin>>choose;
+    cout<<endl;
+    switch(choose){
+        case 0: {
+            main_menu();
+            break;
+        }
+        case 1: {
+            mine_block();
+            break;
+        }
+        case 2: {
+            display_Blockchain();
+            break;
+        }
+        case 3: {
+            display_Ledger();
+            break;
+        }
+        case 4: {
+            display_Mempool();
+            break;
+        }
+        default: {
+            cout<<"This feature is currntly being developed."<<endl;
+            break;
+        }
+        
+    }
+    }
+}
