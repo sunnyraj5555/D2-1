d54e8e0decd1e0696a8e9df49acfc8353375e1e1
sekhar76405 <ssjio5402@gmail.com>
2021-10-04 17:04:23 +0530

    Menu functions declared

--- main/Menu.h
+++ main/Menu.h
@@ -15,5 +15,17 @@ using namespace std;
 #define MENU_H
 
 
+void main_menu();
+void admin_main_menu();
+void user_menu();
+
+void org_list_menu();
+
+
+void post_list_menu();
+
+void donate_to_post_menu(long Post_ID);
+
+
 
 #endif /* MENU_H */
