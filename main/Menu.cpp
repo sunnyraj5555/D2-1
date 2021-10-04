11eebf473db0f6dd40557e19be2d95c967654cc4
sekhar76405 <ssjio5402@gmail.com>
2021-10-04 17:05:35 +0530

    main_menu() defined

--- main/Menu.cpp
+++ main/Menu.cpp
@@ -19,3 +19,32 @@
 
 using namespace std;
 
+
+void main_menu(){
+    cout<<endl;
+    cout<<"\t\t Welcome To D2 - A BlockChain Based Secure Donation System !"<<endl;
+    cout<<"\n\n";
+    cout<<"\t\t1. Login as User "<<endl;
+    cout<<"\t\t2. Login as Admin "<<endl;
+
+    int choose;
+    cout<<"\n\t\t Enter your choice: ";
+    cin>>choose;
+    
+    switch(choose){
+        case 1: {
+            user_menu();
+            break;
+        }
+        case 2: {
+            admin_main_menu();
+            break;
+        }
+           default: {
+            cout<<"\t Invalid Input."<<endl;
+            break;
+        }
+        
+    }
+
+}
