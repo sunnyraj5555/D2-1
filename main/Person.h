d5ae128ba4ba45774782e1d20ccf8cd52d158c10
Aryan Mukerji <aryanmukerji@gmail.com>
2021-10-07 21:03:19 +0530

    Declared User Class and Organization Class in .h file

--- main/Person.h
+++ main/Person.h
@@ -18,10 +18,89 @@ using namespace std;
 
 // ================================= User.cpp ==================================
 
+class User
+{
+    long User_ID;
+    string Name;        
+    string Phone_No;
+    string Email_ID;
+    string Password;
+    int Public_Key, Private_Key;
+    long User_Balance;
+    
+    public:
+    
+    void set_User_ID();
+    void set_User_ID(long uid);   // overloaded for assigning admin ID manually
+    long get_User_ID();
+    void set_user_details(string Name,string Phone_No,string Email_ID,string Password);
+    //void set_user_keys(long Private_Key, long Public_Key);
+    void set_user_private_key(int Private_Key);
+    void set_user_public_key(int Public_Key);
+    
+    void get_User();
+    string get_User_Name();
+    string get_User_Phone_No();
+    string get_User_Email();
+    string get_User_Password();
+    long get_User_Public_Key();
+    long get_User_Private_Key();
+    long get_User_Balance();
 
+    void deduct_User_Balance(long amount);
+};
+
+extern User UserList[N];
+extern long UserList_size ;
+extern long Logged_User_ID;
+extern User Admin;
+
+void admin_init();
+void create_user();
+void add_user_to_list(User &u);
+void display_UserList();
+
+void user_login();
+void login_verify();
+void admin_login();
+
+void generate_keys(User &u);   // this will be in the encryption headerfile
 
 // ============================== Organisation.cpp =============================
 
+class Organization
+{
+    private:
+    
+    string org_name;
+    string org_address;
+    long registration_number; // This is the registration number provided by government
+    string org_phone_number;
+    long Org_ID;    // This is the organiztion ID provided by D2 to all Organization
+  
+    public:
+  
+    void set_Org_ID();
+    long get_Org_ID();
+  
+    void set_org_details(string org_name, string org_address, string org_phone_number );
+    void get_org_details();
+  
+    string get_org_name();
+    string get_org_address();
+    string get_org_phone_number();
+    void set_registration_number(long registration_number);
+    long get_registration_number();
+};
 
+extern Organization OrgList[N];
+extern long OrgList_size;
+
+void display_OrgList();
+void add_org_to_list(Organization &o);
+bool verify_org(long Org_ID);
+void create_org();
+
+void intitialise_OrgList();
 
 #endif /* PERSON_H */
