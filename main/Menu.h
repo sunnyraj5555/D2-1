891a12fcae3d1ef8efa8417c92f2bca7b0ff7e77
Aryan Mukerji <aryanmukerji@gmail.com>
2021-10-25 22:39:16 +0530

    Increased code readability

--- main/Menu.h
+++ main/Menu.h
@@ -14,18 +14,12 @@ using namespace std;
 #ifndef MENU_H
 #define MENU_H
 
-
 void main_menu();
 void admin_main_menu();
 void user_menu();
 
 void org_list_menu();
-
-
 void post_list_menu();
-
 void donate_to_post_menu(long Post_ID);
-
-
 
 #endif /* MENU_H */
