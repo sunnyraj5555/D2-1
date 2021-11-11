3502c14e2b064e2e18e95adbe333978683aabeb8
sekhar76405 <ssjio5402@gmail.com>
2021-11-11 20:04:59 +0530

    updated transaction.h

--- main/Transaction.h
+++ main/Transaction.h
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 #define N 100
@@ -16,6 +18,7 @@ using namespace std;
 
 extern long Mempool_size;
 extern long Ledger_size;
+extern long Transaction_Ids;
 
 class Transaction
 {
@@ -36,10 +39,14 @@ class Transaction
     public:
 
     // this function is used to set the transaction details when a trantransaction is made
-    void set_Transaction(string sender, string reciever, long amount, long Transaction_ID);
+    void set_Transaction(string sender, string reciever, long long amount, long Transaction_ID);
     void set_transaction_msg(string message);
     void set_Transaction_ID();
+    void set_Transaction_ID(long id);
     long get_Transaction_ID();
+    string get_sender();
+    string get_reciever();
+    long long get_amount();
     string get_transaction_msg();
 };
 
@@ -48,11 +55,15 @@ extern Transaction Mempool[N];
 Transaction NewTransaction(string sender, string reciever, long amount);
 void donate_money_UI(long Post_ID);
 
-// client side functions
+//client side functions
 string prepare_transmission_buffer(Transaction &t, long user_priv_key ,long admin_pub_key);
 void send_transaction_from_client_to_server(Transaction &t, long user_priv_key ,long admin_pub_key );
 
 void recieve_transaction_from_client(string recived_buffer, Transaction &t);
 bool verify_transaction(string buffer);
+
+
+
+
 
 #endif /* TRANSACTION_H */
