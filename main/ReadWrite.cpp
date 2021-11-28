87e3aaf95922f02438908132076a2fbe265f1249
sekhar76405 <ssjio5402@gmail.com>
2021-11-28 20:40:01 +0530

    added Check_OrgList_size()

--- main/ReadWrite.cpp
+++ main/ReadWrite.cpp
@@ -102,6 +102,21 @@ void Check_PostList()
     f1.close();
 }
 
+void Check_OrgList_size()
+{
+    ofstream f2;
+    ifstream f1("OrgList_size.txt");
+    
+    if(Is_empty(f1) == true)
+    {
+        f2.open("OrgList_size.txt");
+        f2 << 4;
+    }
+    
+    f2.close();
+    f1.close();
+}
+
 void file_init(){
     // will check if the files are not initalised to values.
 }
